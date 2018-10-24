4853065d8d80683a26176321e212548793536db4
Jose Moreno <morenojr90@yahoo.com>
2018-10-23 21:20:42 -0700

    inital deploy

--- src/config/config.js
+++ src/config/config.js
@@ -6,20 +6,20 @@ import grants from './grants'
 
 const config = {
   firebase_config: {
-    apiKey: 'AIzaSyBQAmNJ2DbRyw8PqdmNWlePYtMP0hUcjpY',
-    authDomain: 'react-most-wanted-3b1b2.firebaseapp.com',
-    databaseURL: 'https://react-most-wanted-3b1b2.firebaseio.com',
-    projectId: 'react-most-wanted-3b1b2',
-    storageBucket: 'react-most-wanted-3b1b2.appspot.com',
-    messagingSenderId: '258373383650'
+    apiKey: "AIzaSyBAq7IAFSebFq6LiOfT5RSBm-6kr0SG8y8",
+    authDomain: "kwestion-me.firebaseapp.com",
+    databaseURL: "https://kwestion-me.firebaseio.com",
+    projectId: "kwestion-me",
+    storageBucket: "kwestion-me.appspot.com",
+    messagingSenderId: "1061143748902"
   },
   firebase_config_dev: {
-    apiKey: 'AIzaSyB31cMH9nJnERC1WCWA7lQHnY08voLs-Z0',
-    authDomain: 'react-most-wanted-dev.firebaseapp.com',
-    databaseURL: 'https://react-most-wanted-dev.firebaseio.com',
-    projectId: 'react-most-wanted-dev',
-    storageBucket: 'react-most-wanted-dev.appspot.com',
-    messagingSenderId: '70650394824'
+     apiKey: "AIzaSyDjcF-YpbZKx_d6hyIX1rh6GvIR-xm3wYg",
+     authDomain: "kwestion-me-dev.firebaseapp.com",
+     databaseURL: "https://kwestion-me-dev.firebaseio.com",
+     projectId: "kwestion-me-dev",
+     storageBucket: "kwestion-me-dev.appspot.com",
+     messagingSenderId: "984944696837"
   },
   firebase_providers: [
     'google.com',
